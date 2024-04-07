fd5f53f9358f05c92c9c27fb93ccaecb5705fca5
Kernel Srinivas <kernel.srinivasan@gmail.com>
2024-04-07 19:43:54 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
-import * as TrackPlayer from './trackPlayer';
 import MusicPlayer from './MusicPlayer';
-
-export * from './constants';
-export * from './hooks';
-export * from './interfaces';
 
 export {
     MusicPlayer
 };
-
-export default TrackPlayer;
